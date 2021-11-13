55e36e8c9ea69fe0c78c2c2d53df405ced789fdb
Merve Bilgin <mrvblgn4@gmail.com>
2021-11-13 20:04:59 +0300

    Proje dosyası ekle.

---
+++ kuyruk/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kuyruk
+{
+    class Queue
+    {
+        private readonly int[] arr;
+        private int rear = -1;
+        private const int front = 0;
+        public Queue(int capacity)
+        {
+            arr = new int[capacity];
+        }
+        public void Enqueue(int value)
+        {
+            if (rear == arr.Length-1)
+                throw new IndexOutOfRangeException("Kuyruk dolu");
+            arr[++rear] = value;
+        }
+
+        public int Dequeue()
+        {
+            if (rear == -1)
+                throw new IndexOutOfRangeException("Kuyruk boş");
+            for (int i = 0; i < rear; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return arr[front];
+        }
+        public int Peek()
+        {
+            if (rear == -1)
+                throw new IndexOutOfRangeException("Kuyruk boş");
+            return arr[front];
+        }
+        public int Count()
+        {
+            return rear + 1;
+        }
+        public void Clear()
+        {
+            rear = -1;
+        }
+        public bool IsEmpty()
+        {
+            return rear == -1;
+        }
+        public IEnumerable<int> GetEnumerator()
+        {
+            for (int i = 0; i <= rear; i++)
+                yield return arr[i];
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue(3);
+            queue.Enqueue(10);
+            queue.Enqueue(11);
+            queue.Enqueue(12);
+            foreach (var item in queue.GetEnumerator())
+                Console.WriteLine(item);
+            Console.WriteLine("");
+            queue.Dequeue();
+            foreach (var item in queue.GetEnumerator())
+                Console.WriteLine(item);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ kuyruk/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("kuyruk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kuyruk")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("98df84b9-a7a5-4a82-8db6-cd5fb69cd980")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
